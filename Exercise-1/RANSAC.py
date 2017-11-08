7abc8db07cad0e47ca395822808d2451f115be6d
Vicuko <victorc.fernandez@gmail.com>
2017-11-08 17:51:56 +0100

    Add voxel selection, also known as leaf, in exercise 1

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -6,13 +6,17 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
 # Voxel Grid filter
-
+# Note: this (1) is a poor choice of leaf size   
+# Experiment and find the appropriate size!
+LEAF_SIZE = 1   
 
 # PassThrough filter
-
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
 
 # RANSAC plane segmentation
-
+cloud_filtered = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
 
 # Extract inliers
 
