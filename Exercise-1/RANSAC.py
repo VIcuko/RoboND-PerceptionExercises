e6e9186b86022beeaef0bc2c99fbce71eabd3d25
Vicuko <victorc.fernandez@gmail.com>
2017-11-08 20:34:01 +0100

    Extract inliers to filter only table layer

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -7,7 +7,8 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 # Create a VoxelGrid filter object for our input point cloud
 vox = cloud.make_voxel_grid_filter()
 
-# Voxel Grid filter
+# Voxel Grid filter:
+
 # Note: this (1) is a poor choice of leaf size   
 # Experiment and find the appropriate size!
 LEAF_SIZE = 0.01   
@@ -15,16 +16,11 @@ LEAF_SIZE = 0.01
 # Set the voxel (or leaf) size  
 vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
 
-# Extract inliers
-# RANSAC plane segmentation
 cloud_filtered = vox.filter()
 filename = 'voxel_downsampled.pcd'
-
-# Save pcd for table
-# pcl.save(cloud, filename)
 pcl.save(cloud_filtered, filename)
 
-# PassThrough filter
+# PassThrough filter:
 # Create a PassThrough filter object.
 passthrough = cloud_filtered.make_passthrough_filter()
 
@@ -35,10 +31,41 @@ axis_min = 0.6
 axis_max = 1.1
 passthrough.set_filter_limits(axis_min, axis_max)
 
-# Extract outliers
 # Finally use the filter function to obtain the resultant point cloud. 
 cloud_filtered = passthrough.filter()
 filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
+
+# RANSAC plane segmentation:
+
+# Create the segmentation object
+seg = cloud_filtered.make_segmenter()
+
+# Set the model you wish to fit 
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+# Max distance for a point to be considered fitting the model
+# Experiment with different values for max_distance 
+# for segmenting the table
+max_distance = 0.001
+seg.set_distance_threshold(max_distance)
+
+# Call the segment function to obtain set of inlier indices and model coefficients
+inliers, coefficients = seg.segment()
+
+# Extract inliers:
+
+extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+filename = 'extracted_inliers.pcd'
+pcl.save(extracted_inliers, filename)
+
+# Save pcd for table
+# pcl.save(cloud, filename)
+
+# Extract outliers
+
 
 # Save pcd for tabletop objects
-pcl.save(cloud_filtered, filename)
+
+
