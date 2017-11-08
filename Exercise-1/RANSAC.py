cca2d42093f04933b7d53818e540500be16be435
Vicuko <victorc.fernandez@gmail.com>
2017-11-08 19:49:19 +0100

    Apply passthrough filter to point cloud

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -12,7 +12,16 @@ vox = cloud.make_voxel_grid_filter()
 # Experiment and find the appropriate size!
 LEAF_SIZE = 0.01   
 
+# Set the voxel (or leaf) size  
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+# RANSAC plane segmentation
+cloud_filtered = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
+
 # PassThrough filter
+# Create a PassThrough filter object.
 passthrough = cloud_filtered.make_passthrough_filter()
 
 # Assign axis and range to the passthrough filter object.
@@ -22,10 +31,12 @@ axis_min = 0
 axis_max = 2
 passthrough.set_filter_limits(axis_min, axis_max)
 
-# Finally use the filter function to obtain the resultant point cloud. 
-cloud_filtered = passthrough.filter()
-filename = 'pass_through_filtered.pcd'
-pcl.save(cloud_filtered, filename)
+# Assign axis and range to the passthrough filter object.
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0
+axis_max = 2
+passthrough.set_filter_limits(axis_min, axis_max)
 
 # Extract inliers
 
